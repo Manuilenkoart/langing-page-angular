0694435ede4fc601821143be8e0eaa58fe6fba4d
manuilenkoart <manuilenkoart@gmail.com>
2021-03-30 10:17:33 +0300

    add server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../dist/landing-page-angular")));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname,"../dist/landing-page-angular/index.html")));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
