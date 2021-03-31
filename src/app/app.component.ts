23dd4fca30589265e4ace8375c43b0912e40fe62
manuilenkoart <manuilenkoart@gmail.com>
2021-04-01 00:10:23 +0300

    update mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {faPinterestP, faFacebookF, faInstagram, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faHeart, faCommentAlt, faEye} from '@fortawesome/free-regular-svg-icons';
+import {faLongArrowAltUp} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'landing-page-angular';
+  faFacebookF = faFacebookF;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
+  faPinterestP = faPinterestP;
+  faHeart = faHeart;
+  faCommentAlt = faCommentAlt;
+  faEye = faEye;
+  faLongArrowAltUp = faLongArrowAltUp;
 }
